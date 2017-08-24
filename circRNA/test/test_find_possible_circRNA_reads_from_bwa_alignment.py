dd4acb4c053387205749a0e15a24cd30b77a1c73
Brian Yee <bay001@tscc-login1.sdsc.edu>
2017-08-24 16:44:03 -0700

    fixed test to just test for duplicates

--- circRNA/test/test_find_possible_circRNA_reads_from_bwa_alignment.py
+++ circRNA/test/test_find_possible_circRNA_reads_from_bwa_alignment.py
@@ -3,16 +3,23 @@ import pandas as pd
 
 def duped_sam_1():
     samfile = 'test/data/226_igf2bp2.sai.sorted.read.sam'
-    return fpc.parse_sam_file(samfile)
+    df = pd.read_table(
+        samfile, sep='\t', comment='@', names=[
+            'rname', 'flag', 'junc_name', 'pos', 'mapq', 'cigar',
+            'rnext', 'pnext', 'tlen', 'seq', 'qual', 'XT', 'NM',
+            'X0', 'X1', 'XM', 'XO', 'XG', 'MDZ'
+        ]
+    )
+    return df
 
 def test_check_for_duplicate_reads_1():
     print("assume function keeps only unique reads")
-    df, _ = duped_sam_1()
+    df = duped_sam_1()
     deduped = fpc.check_for_duplicate_reads(df)
     assert deduped.shape[0] == 1
 
 def test_check_for_duplicate_reads_2():
     print("assume function removes both duplicated reads")
-    df, _ = duped_sam_1()
+    df = duped_sam_1()
     deduped = fpc.check_for_duplicate_reads(df)
     assert deduped.shape[0] == 0
